fdf0dfd0f9120c017c4ad2e7dfbd15a44693f081
ndxzzy <65444105+ndxzzy@users.noreply.github.com>
2024-03-30 18:49:07 +0800

    Update privacy-protection.user.js

--- privacy-protection.user.js
+++ privacy-protection.user.js
@@ -1,11 +1,13 @@
 // ==UserScript==
 // @name         雨云截图隐私保护脚本
 // @namespace    http://tampermonkey.net/
-// @version      0.6
+// @version      0.7
 // @description  给包含特定隐私内容的元素添加模糊效果，并提供开关按钮控制
 // @author       ndxzzy,ChatGPT
 // @match        *://app.rainyun.com/*
 // @grant        none
+// @updateURL    https://github.com/rainyun-space/privacy-protection/raw/main/privacy-protection.user.js
+// @downloadURL  https://github.com/rainyun-space/privacy-protection/raw/main/privacy-protection.user.js
 // ==/UserScript==
 
 (function() {
@@ -64,6 +66,24 @@
         if (!privacyProtectionEnabled) {
             return;
         }
+        var h4Elements = document.querySelectorAll('h4');
+        h4Elements.forEach(h4Element => {
+            if (h4Element.textContent.includes('公网IP列表') ||
+                h4Element.textContent.includes('发票抬头列表') ||
+                h4Element.textContent.includes('我的发票') ||
+                h4Element.textContent.includes('发起提现') ||
+                h4Element.textContent.includes(' 域名管理') ||
+                h4Element.textContent.includes(' 我的模板') ||
+                h4Element.textContent.includes(' 域名管理') ||
+                h4Element.textContent.includes('NAT端口映射管理') ||
+                h4Element.textContent.includes('绑定支付宝') ||
+                h4Element.textContent.includes('API秘钥')) {
+                var divParent = h4Element.parentNode;
+                if (divParent.tagName === 'DIV') {
+                    divParent.style.filter = 'blur(5px)';
+                }
+            }
+        });
         var elements = document.querySelectorAll('p, td, div');
         elements.forEach(element => {
             if (element.tagName === 'P') {
@@ -76,16 +96,6 @@
                 if (element.querySelector('small') && element.querySelector('small').textContent.includes('公网 IP 地址：')) {
                     element.style.filter = 'blur(5px)';
                 } else if (element.querySelector('small') && element.querySelector('small').textContent.includes('面板主账户：')) {
-                    element.style.filter = 'blur(5px)';
-                }
-            } else if (element.tagName === 'DIV') {
-                if (element.querySelector('h4') && element.querySelector('h4').textContent.includes('公网IP列表') ||
-                    element.querySelector('h4') && element.querySelector('h4').textContent.includes('NAT端口映射管理') ||
-                    element.querySelector('h4') && element.querySelector('h4').textContent.includes('绑定支付宝') ||
-                    element.querySelector('h4') && element.querySelector('h4').textContent.includes('API秘钥')) {
-                    element.style.filter = 'blur(5px)';
-                }
-                if (element.querySelector('small') && element.querySelector('small').textContent.includes('标签')) {
                     element.style.filter = 'blur(5px)';
                 }
             }
